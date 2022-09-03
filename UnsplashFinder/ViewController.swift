1e3132cf9f373ae11946270f7a1746c833757dc9
kirill_off <kirilloffkostya@gmail.com>
2022-09-03 13:22:14 +0300

    Initial Commit

---
+++ UnsplashFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashFinder
+//
+//  Created by Konstantin Kirillov on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
